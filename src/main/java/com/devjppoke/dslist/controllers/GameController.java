24fce5b61fa98fcf500512f01bffa227e233b545
JPPOKE <jppokedev@gmail.com>
2024-11-17 06:54:32 -0300

    Game repositoty, service, controller

---
+++ src/main/java/com/devjppoke/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.devjppoke.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devjppoke.dslist.dto.GameMinDTO;
+import com.devjppoke.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+	
+}
